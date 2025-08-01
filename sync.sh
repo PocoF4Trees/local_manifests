c1ce12fe840c08be04995872054bf50bb4ba0dd0
olzhas0986 <89378221+Olzhas-Kdyr@users.noreply.github.com>
2025-08-01 19:30:43 +0500

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -12,13 +12,13 @@ packages/apps/ViPER4AndroidFX
 rm -rf "${mf[@]}"
 
 # Deivce Tree
-git clone https://github.com/PocoF4Trees/device_xiaomi_munch -b 16 device/xiaomi/munch
+git clone https://github.com/PocoF4Trees/device_xiaomi_munch -b infinity device/xiaomi/munch
 
 # Vendor Tree
 git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
 
 # Munch FW
-#git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 # Kernel Tree
 git clone https://github.com/PocoF4Trees/kernel_xiaomi_munch -b munch kernel/xiaomi/munch
